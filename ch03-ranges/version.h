5d997cccac66a2b6df520c05a188af1cfb1878bf
Dima Kochetov <dima.kochetov.70@gmail.com>
2025-01-18 13:59:16 +0500

    Add version concepts for static dispatch
    Takeaways:
    - concepts can be implemented on values, not just on types, which is
    fairly reasonable;
    - there's another way to do static dispatch, besides overloading and
    specializing templates - through requires-clauses

--- ch03-ranges/version.h
+++ ch03-ranges/version.h
@@ -4,3 +4,15 @@ enum class Version {
   Iterator,
   Ranges,
 };
+
+template <Version version>
+concept VersionIterator = (version == Version::Iterator);
+
+template <Version version>
+concept VersionRanges = (version == Version::Ranges);
+
+static_assert(VersionIterator<Version::Iterator>);
+static_assert(!VersionIterator<Version::Ranges>);
+
+static_assert(VersionRanges<Version::Ranges>);
+static_assert(!VersionRanges<Version::Iterator>);
